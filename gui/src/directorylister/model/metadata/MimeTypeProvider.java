5008961e73ec3623e81a9d81fb3ed0c30c87bd0f
ginnungagap <ginnungagap@6369debb-3c36-0410-839e-aeae4d9b6edc>
2007-08-19 16:17:25 +0000

    Added use of JMimeMagic library for mime type detection

--- gui/src/directorylister/model/metadata/MimeTypeProvider.java
+++ gui/src/directorylister/model/metadata/MimeTypeProvider.java
@@ -1,7 +1,18 @@
 package directorylister.model.metadata;
 
+import net.sf.jmimemagic.Magic;
+import net.sf.jmimemagic.MagicException;
+import net.sf.jmimemagic.MagicMatch;
+import net.sf.jmimemagic.MagicMatchNotFoundException;
+import net.sf.jmimemagic.MagicParseException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import javax.activation.MimetypesFileTypeMap;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -11,18 +22,78 @@ import java.util.Collection;
  */
 // TODO: Improve capabilities of the code.
 public class MimeTypeProvider implements MetadataProvider {
+
+    private static final Log logger = LogFactory.getLog(MimeTypeProvider.class);
+    private static final int BUFFER_SIZE = 2 << 16;
+
+    public MimeTypeProvider() {
+
+    }
+
     public Collection<FileEntryMetaData> getMetadata(final File file) {
-        final String mimeType;
+        String mimeType;
         if (file.isDirectory()) {
             mimeType = "Directory";
-        }
-        else {
-            mimeType = MimetypesFileTypeMap.getDefaultFileTypeMap().getContentType(file);
+        } else {
+
+            mimeType = getMimeTypeUsingJMimeMagic(file);
+
+            if (null == mimeType) {
+                mimeType = getMimeTypeUsingJAF(file);
+            }
         }
 
         final FileEntryMetaData fileEntryMetaData = new FileEntryMetaData();
         fileEntryMetaData.setKey(new MimeTypeMetaDataKey());
         fileEntryMetaData.setValue(new MimeTypeMetaDataValue(mimeType));
         return Arrays.asList(fileEntryMetaData);
+    }
+
+    private String getMimeTypeUsingJMimeMagic(final File file) {
+        // TODO: 
+        if(true){
+            return null;
+        }
+        String mimeType = null;
+        try {
+            final byte[] content = readFirstBytes(file);
+            final MagicMatch magicMatch = Magic.getMagicMatch(content);
+            mimeType = magicMatch.getMimeType();
+        } catch (MagicParseException e) {
+            logger.warn(e);
+        } catch (MagicMatchNotFoundException e) {
+            logger.warn(e);
+        } catch (MagicException e) {
+            logger.warn(e);
+        }
+        return mimeType;
+    }
+
+    private byte[] readFirstBytes(final File file) {
+        final long fileSize = file.length();
+        final int bufferSize = (int) (fileSize > BUFFER_SIZE ? BUFFER_SIZE : fileSize);
+        final byte[] buffer = new byte[bufferSize];
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+            inputStream.read(buffer);
+        }
+        catch (IOException e) {
+            logger.error(e);
+        }
+        finally {
+            if (null != inputStream) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    logger.error(e);
+                }
+            }
+        }
+        return buffer;
+    }
+
+    private String getMimeTypeUsingJAF(final File file) {
+        return MimetypesFileTypeMap.getDefaultFileTypeMap().getContentType(file);
     }
 }
